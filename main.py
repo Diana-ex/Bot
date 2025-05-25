817f2109848eefaeb5e59f033bbd5c900c98f0a1
диана <parashutist376@gmail.com>
2025-05-25 23:21:25 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+import vk_api
+import os
+
+app = Flask(__name__)
+
+GROUP_TOKEN = os.getenv("GROUP_TOKEN")
+vk_session = vk_api.VkApi(token=GROUP_TOKEN)
+vk = vk_session.get_api()
+
+@app.route('/', methods=['POST'])
+def main():
+    data = request.json
+
+    if data['type'] == 'confirmation':
+        return 'a592aed9'  # Код подтверждения из ВК
+
+    elif data['type'] == 'message_new':
+        user_id = data['object']['peer_id']
+        message_text = data['object']['text']
+
+        # Отправляем ответ пользователю
+        vk.messages.send(
+            peer_id=user_id,
+            message=f"Вы написали: {message_text}",
+            random_id=0
+        )
+
+    return 'ok'
+
+if __name__ == '__main__':
+    app.run()
